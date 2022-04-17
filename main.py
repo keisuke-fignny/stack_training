9e8e867fbc8f61b963283cd8663a226460a962ac
@satomi1 <satomi@fignny.co.jp>
2022-04-17 23:01:14 +0900

    change

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from myStack import MyStack
 stack = MyStack()
 
 print(f'stacked {stack.push("hello")}')
-print(f'stacked {stack.push("good evening")}')
+print(f'stacked {stack.push("good-evening")}')
 print(f'stacked {stack.push("zzzzz")}')
+
+print(f'-----------------------------------')
 
 print(f'popped {stack.pop()}')
 print(f'popped {stack.pop()}')
